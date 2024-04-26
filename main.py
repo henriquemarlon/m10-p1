c8226c1575eb18ba112ad6809cb1e2da6b36bea0
Henrique Marlon <henriquemarloncs@gmail.com>
2024-04-26 18:56:24 -0300

    fix dockerfile

--- main.py
+++ main.py
@@ -115,4 +115,4 @@ def updateTask():
     return jsonify({"status": "success"})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000, host="0.0.0.0")
+    app.run(debug=True, port=3000, host="0.0.0.0")
